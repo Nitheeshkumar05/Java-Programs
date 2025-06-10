0a6377ceea9cfd4d44e5ea3518ccdba44cc5e209
Nitheeshkumar05 <nitheeshkumarcodes2004@gmail.com>
2025-06-10 20:07:22 +0530

    Create Factorial.java

---
+++ Basic Problems/Factorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class factorial {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        int a = sc.nextInt();
+        int fact = 1;
+
+        while(a != 0){
+            fact *=  a;
+            a--;   
+        }
+        System.out.println("Factorial is : " +fact);
+    }
+
+    
+}
