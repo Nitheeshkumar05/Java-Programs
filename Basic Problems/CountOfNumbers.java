226a10ee322c6ba46912939eb58cda0ea2e9131d
NITHEESH KUMAR M <nitheeshkumarcodes2004@gmail.com>
2025-07-18 20:15:09 +0530

    Create CountOfNumbers.java

---
+++ Basic Problems/CountOfNumbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class NumbersCount {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int digit = (int)Math.log10(n) + 1;
+        // int count = 0;
+       
+        System.out.println(digit);
+
+    }
+    
+}
